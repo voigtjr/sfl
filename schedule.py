885e5d7369cd7bbd953d9b1d977d291372df7d4d
Jonathan Voigt <voigtjr@gmail.com>
2014-09-01 20:19:20 -0400

    new draft order

--- schedule.py
+++ schedule.py
@@ -5,16 +5,23 @@ import json
 import sys
 import pprint
 
-def verify():
-    schedule = None
-    with open('schedule.json') as f:
-        schedule = json.load(f)
+def load_json(filename):
+    ret = None
+    with open(filename) as f:
+        ret = json.load(f)
 
-    if not schedule:
-        print "invalid schedule"
+    if not ret:
+        print "invalid", filename
         sys.exit(1)
 
+    return ret
+
+def validate():
+    schedule = load_json('schedule.json')
     pprint.pprint(schedule)
+
+    draft_order = load_json('draft_order.json')
+    pprint.pprint(draft_order)
 
 def parse_args():
     p = argparse.ArgumentParser()
@@ -24,7 +31,7 @@ def parse_args():
 def main():
     args = parse_args()
 
-    verify()
+    validate()
 
 if __name__ == '__main__':
     main()
